d596617c41e6da1fb2daaae9d123779356972ecb
Chonlasith Jucksriporn <chonlasith@gmail.com>
2018-08-17 14:12:55 +0700

    Add rand formula

---
+++ FormulaTemplate/rand.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env bash
+
+# rand.sh <version>
+
+VERSION=$1
+URL="https://github.com/chonla/rand/archive/${VERSION}.tar.gz"
+CHECKSUM=$(curl -sL ${URL} | shasum -a 256 | awk '{print $1;}')
+
+tmpl="class rand < Formula
+    desc \"Randomizer cli\"
+    homepage \"https://github.com/chonla/rand\"
+    url \"${URL}\"
+    sha256 \"${CHECKSUM}\"
+    depends_on \"go\" => :build
+
+    def install
+        system \"bash\", \"build.sh\"
+        bin.install \"/tmp/.gobuild/bin/rand\" => \"rand\"
+    end
+
+    test do
+        system \"#{bin}/rand\", \"-v\"
+    end
+end"
+
+echo "$tmpl" > rand.rb
---
+++ HomebrewFormula/rand.rb
@@ -0,0 +1,16 @@
+class rand < Formula
+    desc "Randomizer cli"
+    homepage "https://github.com/chonla/rand"
+    url "https://github.com/chonla/rand/archive/0.1.0.tar.gz"
+    sha256 "e4cdb443ab58938363b5ee5d2c5d5e19160e48fca52d32ecfbfdec8e42583c7e"
+    depends_on "go" => :build
+
+    def install
+        system "bash", "build.sh"
+        bin.install "/tmp/.gobuild/bin/rand" => "rand"
+    end
+
+    test do
+        system "#{bin}/rand", "-v"
+    end
+end
