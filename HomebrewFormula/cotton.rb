47d3b2f601b821715c9e846ffbb861ff43cb8031
Chonlasith Jucksriporn <chonlasith@gmail.com>
2020-01-27 23:40:33 +0700

    Update cotton version

--- HomebrewFormula/cotton.rb
+++ HomebrewFormula/cotton.rb
@@ -1,8 +1,8 @@
 class Cotton < Formula
     desc "Markdown Test Specification Runner"
     homepage "https://github.com/chonla/cotton"
-    url "https://github.com/chonla/cotton/archive/0.3.6.tar.gz"
-    sha256 "987864eca79d979834778953867f91e34d3abdf7540b0760f99429e5508fce55"
+    url "https://github.com/chonla/cotton/archive/0.3.7.tar.gz"
+    sha256 "5bcdc0b2258a1156e89cde7e4ff89719a374b78b386ef26e7b15e385bf394f0c"
     depends_on "go" => :build
 
     def install
