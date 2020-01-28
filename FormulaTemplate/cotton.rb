2da40393f01a0284fbf207740e067f2859dc1889
Chonlasith Jucksriporn <chonlasith@gmail.com>
2020-01-28 19:18:14 +0700

    Update cotton

--- FormulaTemplate/cotton.rb
+++ FormulaTemplate/cotton.rb
@@ -1,8 +1,8 @@
 class Cotton < Formula
     desc "Markdown Test Specification Runner"
     homepage "https://github.com/chonla/cotton"
-    url "https://github.com/chonla/cotton/archive/0.3.7.tar.gz"
-    sha256 "5bcdc0b2258a1156e89cde7e4ff89719a374b78b386ef26e7b15e385bf394f0c"
+    url "https://github.com/chonla/cotton/archive/0.3.8.tar.gz"
+    sha256 "e9bb91996e006156b610f3ae287be9e84c3249191358ec7f6a3dfd647a6d8fbb"
     depends_on "go" => :build
 
     def install
