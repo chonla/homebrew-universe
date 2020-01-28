db4213d62a801357b0b33caef7d3633532e3fe89
Chonlasith Jucksriporn <chonlasith@gmail.com>
2020-01-28 19:25:12 +0700

    Fix sha

--- FormulaTemplate/cotton.rb
+++ FormulaTemplate/cotton.rb
@@ -2,7 +2,7 @@ class Cotton < Formula
     desc "Markdown Test Specification Runner"
     homepage "https://github.com/chonla/cotton"
     url "https://github.com/chonla/cotton/archive/0.3.8.tar.gz"
-    sha256 "e9bb91996e006156b610f3ae287be9e84c3249191358ec7f6a3dfd647a6d8fbb"
+    sha256 "8aa6def8635632571e038568ec55756bf4d71b165429223ba0cfb21cc29b6cc6"
     depends_on "go" => :build
 
     def install
