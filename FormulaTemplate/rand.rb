7abd43a753d63ed81ee5e7cc4f0404b147d6029a
Chonlasith Jucksriporn <chonlasith@gmail.com>
2022-04-08 16:20:32 +0700

    chore: Update rand version

--- FormulaTemplate/rand.rb
+++ FormulaTemplate/rand.rb
@@ -1,9 +1,9 @@
-# v0.2.1
+# v0.2.2
 class Rand < Formula
     desc "Randomizer cli"
     homepage "https://github.com/chonla/rand"
-    url "https://github.com/chonla/rand/archive/v0.2.1.tar.gz"
-    sha256 "484e1a8a24566e5c849009f9a4b301d2c920a95f33e67dc5f2916ef2bbd3b08b"
+    url "https://github.com/chonla/rand/archive/v0.2.2.tar.gz"
+    sha256 "8cfc2453b326490d793add29282c9b9ef0a5b1b9c2c8448e8bb73acff0ba94ff"
     depends_on "go" => :build
 
     def install
