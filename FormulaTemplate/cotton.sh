b0af24259137a0354aa377beec4bc8dccafbf085
Chonlasith Jucksriporn <chonlasith@gmail.com>
2024-07-11 16:11:53 +0700

    feat: cotton 1.0.0 release

--- FormulaTemplate/cotton.sh
+++ FormulaTemplate/cotton.sh
@@ -3,20 +3,80 @@
 # cotton.sh <version>
 
 VERSION=$1
-URL="https://github.com/chonla/cotton/archive/${VERSION}.tar.gz"
-CHECKSUM=$(curl -sL ${URL} | shasum -a 256 | awk '{print $1;}')
+
+verify_release() {
+    local version="$1"
+    local os="$2"
+    local arch="$3"
+
+    url="https://github.com/chonla/cotton/releases/download/${version}/cotton-${version}-${os}-${arch}.tar.gz"
+    local checksum_url="${url}.sha256"
+
+    echo -n "collecting release data for ${os} ${arch} ..."
+    file_checksum=$(curl -sL ${url} | shasum -a 256 | awk '{print $1;}')
+    echo ""
+
+    echo -n "verifying release data for ${os} ${arch} ..."
+    local expected_checksum=$(curl -sL ${checksum_url})
+    if [ "$file_checksum" == "$expected_checksum" ]; then
+        echo " passed"
+        return 0
+    else
+        echo " failed"
+        echo "  ${expected_checksum} is expected"
+        echo "  ${file_checksum} is found"
+        return 1
+    fi
+}
+
+echo "generating homebrew formula for cotton ${VERSION}"
+
+if verify_release "${VERSION}" "darwin" "arm64"; then
+    DARWIN_ARM64_URL=${url}
+    DARWIN_ARM64_CHECKSUM=${file_checksum}
+else
+    exit 1
+fi
+
+if verify_release "${VERSION}" "darwin" "amd64"; then
+    DARWIN_AMD64_URL=${url}
+    DARWIN_AMD64_CHECKSUM=${file_checksum}
+else
+    exit 1
+fi
+
+
+if verify_release "${VERSION}" "linux" "amd64"; then
+    LINUX_AMD64_URL=${url}
+    LINUX_AMD64_CHECKSUM=${file_checksum}
+else
+    exit 1
+fi
+
 
 tmpl="# ${VERSION}
 class Cotton < Formula
     desc \"Markdown Test Specification Runner\"
     homepage \"https://github.com/chonla/cotton\"
-    url \"${URL}\"
-    sha256 \"${CHECKSUM}\"
-    depends_on \"go\" => :build
+    on_macos do
+        on_arm do
+            url \"${DARWIN_ARM64_URL}\"
+            sha256 \"${DARWIN_ARM64_CHECKSUM}\"
+        end
+        on_intel do
+            url \"${DARWIN_AMD64_URL}\"
+            sha256 \"${DARWIN_AMD64_CHECKSUM}\"
+        end
+    end
+    on_linux do
+        on_intel do
+            url \"${LINUX_AMD64_URL}\"
+            sha256 \"${LINUX_AMD64_CHECKSUM}\"
+        end
+    end
 
     def install
-        system \"bash\", \"build.sh\"
-        bin.install \"/tmp/.gobuild/bin/cotton\" => \"cotton\"
+        bin.install \"cotton\" => \"cotton\"
     end
 
     test do
@@ -25,3 +85,5 @@ class Cotton < Formula
 end"
 
 echo "$tmpl" > cotton.rb
+
+echo "cotton.rb is generated."
--- HomebrewFormula/cotton.rb
+++ HomebrewFormula/cotton.rb
@@ -1,14 +1,26 @@
-# 0.4.0
+# 1.0.0
 class Cotton < Formula
     desc "Markdown Test Specification Runner"
     homepage "https://github.com/chonla/cotton"
-    url "https://github.com/chonla/cotton/archive/0.5.0.tar.gz"
-    sha256 "c68c837a40c29e32ed939e5e34e157842c51115cb2851b6ce7c370d319d77f54"
-    depends_on "go" => :build
+    on_macos do
+        on_arm do
+            url "https://github.com/chonla/cotton/releases/download/1.0.0/cotton-1.0.0-darwin-arm64.tar.gz"
+            sha256 "a0d8ff747b3981947729fb02fe1f6730e0ba4ee0b3d03501f2687a1bced361c5"
+        end
+        on_intel do
+            url "https://github.com/chonla/cotton/releases/download/1.0.0/cotton-1.0.0-darwin-amd64.tar.gz"
+            sha256 "898f39fdb26b69795c0de8b9a2ad8e3618b5db9a9c4c68aff86c5cb99ce3b554"
+        end
+    end
+    on_linux do
+        on_intel do
+            url "https://github.com/chonla/cotton/releases/download/1.0.0/cotton-1.0.0-linux-amd64.tar.gz"
+            sha256 "6a985a9a8ebcf03965366e97540dddb7488d0829d9fa3bfcd27a187ab80321a8"
+        end
+    end
 
     def install
-        system "bash", "build.sh"
-        bin.install "/tmp/.gobuild/bin/cotton" => "cotton"
+        bin.install "cotton" => "cotton"
     end
 
     test do
