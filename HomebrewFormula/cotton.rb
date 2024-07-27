047187170327df910c6c96d8543ff92395bfdb8d
Chonlasith Jucksriporn <chonlasith@gmail.com>
2024-07-27 21:39:05 +0700

    feat: cotton 1.1.1

--- HomebrewFormula/cotton.rb
+++ HomebrewFormula/cotton.rb
@@ -1,21 +1,21 @@
-# 1.1.0
+# 1.1.1
 class Cotton < Formula
     desc "Markdown Test Specification Runner"
     homepage "https://github.com/chonla/cotton"
     on_macos do
         on_arm do
-            url "https://github.com/chonla/cotton/releases/download/1.1.0/cotton-1.1.0-darwin-arm64.tar.gz"
-            sha256 "3fe28a6ee381eaf172f0eef6f64e591428a2bd222942295b18775a9508594c22"
+            url "https://github.com/chonla/cotton/releases/download/1.1.1/cotton-1.1.1-darwin-arm64.tar.gz"
+            sha256 "c9d65c7157c8fc4cb285edcb48ae28e24b8952d5714e34ee70d678e7e4f3f94b"
         end
         on_intel do
-            url "https://github.com/chonla/cotton/releases/download/1.1.0/cotton-1.1.0-darwin-amd64.tar.gz"
-            sha256 "d1872c287a8a2a7a30e705ae28f3a7b3a5f473649bf4f6178fdd5e428d39a65a"
+            url "https://github.com/chonla/cotton/releases/download/1.1.1/cotton-1.1.1-darwin-amd64.tar.gz"
+            sha256 "e005fca36ae32cf83b5761246b59a5fffcd92034f9017a6e8232a2a4240230d6"
         end
     end
     on_linux do
         on_intel do
-            url "https://github.com/chonla/cotton/releases/download/1.1.0/cotton-1.1.0-linux-amd64.tar.gz"
-            sha256 "c38a5c92ee1c0ae5cd4eebda6dfd5a81a5030ff014f6ab980e3b79a755df41e0"
+            url "https://github.com/chonla/cotton/releases/download/1.1.1/cotton-1.1.1-linux-amd64.tar.gz"
+            sha256 "c7bfc3548d5d6061900d4f7551d70cb6ab4c686a620b15d24bb87fa203335029"
         end
     end
 
